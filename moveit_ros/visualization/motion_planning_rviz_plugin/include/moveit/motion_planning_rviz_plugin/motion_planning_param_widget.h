25a9ba17ad4497ac549124e6b04356cdb07b2f27
Tyler Weaver <tyler@picknik.ai>
2020-10-13 10:32:15 -0600

    Rebase from 'upstream'

--- moveit_ros/visualization/motion_planning_rviz_plugin/include/moveit/motion_planning_rviz_plugin/motion_planning_param_widget.h
+++ moveit_ros/visualization/motion_planning_rviz_plugin/include/moveit/motion_planning_rviz_plugin/motion_planning_param_widget.h
@@ -37,17 +37,13 @@
 #pragma once
 
 #include <moveit/macros/class_forward.h>
-#include <moveit/macros/diagnostics.h>
-DIAGNOSTIC_PUSH
-SILENT_UNUSED_PARAM
 #include <rviz/properties/property_tree_widget.h>
-DIAGNOSTIC_POP
 
 namespace moveit
 {
 namespace planning_interface
 {
-MOVEIT_CLASS_FORWARD(MoveGroupInterface);
+MOVEIT_CLASS_FORWARD(MoveGroupInterface);  // Defines MoveGroupInterfacePtr, ConstPtr, WeakPtr... etc
 }
 }  // namespace moveit
 
