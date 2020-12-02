095e6bc511f688a06fe7eda68e3a4f2a07540ef1
Tyler Weaver <tylerjw@gmail.com>
2020-12-02 10:21:40 -0700

    Rebase from 'upstream'

--- moveit_setup_assistant/src/widgets/kinematic_chain_widget.h
+++ moveit_setup_assistant/src/widgets/kinematic_chain_widget.h
@@ -38,12 +38,13 @@
 #define MOVEIT_MOVEIT_SETUP_ASSISTANT_WIDGETS_KINEMATIC_CHAIN_WIDGET_
 
 #include <QWidget>
-#include <QLabel>
-#include <QTreeWidget>
+class QLabel;
+class QLineEdit;
+class QTreeWidget;
+class QTreeWidgetItem;
 
 #ifndef Q_MOC_RUN
 #include <moveit/setup_assistant/tools/moveit_config_data.h>
-#include <ros/ros.h>
 #endif
 
 namespace moveit_setup_assistant
