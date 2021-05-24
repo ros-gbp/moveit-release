91a81c8a4f76ed9151749a5d40063a4eb4287b49
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2021-05-24 09:41:52 +0200

    Rebase from 'upstream'

--- moveit_ros/moveit_servo/include/moveit_servo/servo_calcs.h
+++ moveit_ros/moveit_servo/include/moveit_servo/servo_calcs.h
@@ -39,7 +39,10 @@
 #pragma once
 
 // C++
+#include <condition_variable>
 #include <mutex>
+#include <thread>
+#include <atomic>
 
 // ROS
 #include <control_msgs/JointJog.h>
@@ -69,10 +72,7 @@ public:
   ServoCalcs(ros::NodeHandle& nh, ServoParameters& parameters,
              const planning_scene_monitor::PlanningSceneMonitorPtr& planning_scene_monitor);
 
-  ~ServoCalcs()
-  {
-    timer_.stop();
-  }
+  ~ServoCalcs();
 
   /** \brief Start the timer where we do work and publish outputs */
   void start();
@@ -105,9 +105,18 @@ public:
    */
   void changeRobotLinkCommandFrame(const std::string& new_command_frame);
 
+  // Give test access to private/protected methods
+  friend class ServoFixture;
+
 private:
-  /** \brief Timer method */
-  void run(const ros::TimerEvent& timer_event);
+  /** \brief Run the main calculation loop */
+  void mainCalcLoop();
+
+  /** \brief Do calculations for a single iteration. Publish one outgoing command */
+  void calculateSingleIteration();
+
+  /** \brief Stop the currently running thread */
+  void stop();
 
   /** \brief Do servoing calculations for Cartesian twist commands. */
   bool cartesianServoCalcs(geometry_msgs::TwistStamped& cmd, trajectory_msgs::JointTrajectory& joint_trajectory);
@@ -257,8 +266,6 @@ private:
   trajectory_msgs::JointTrajectoryConstPtr last_sent_command_;
 
   // ROS
-  ros::Timer timer_;
-  ros::Duration period_;
   ros::Subscriber joint_state_sub_;
   ros::Subscriber twist_stamped_sub_;
   ros::Subscriber joint_cmd_sub_;
@@ -270,9 +277,13 @@ private:
   ros::ServiceServer control_dimensions_server_;
   ros::ServiceServer reset_servo_status_;
 
+  // Main tracking / result publisher loop
+  std::thread thread_;
+  bool stop_requested_;
+
   // Status
   StatusCode status_ = StatusCode::NO_WARNING;
-  bool paused_ = false;
+  std::atomic<bool> paused_;
   bool twist_command_is_stale_ = false;
   bool joint_command_is_stale_ = false;
   bool ok_to_publish_ = false;
@@ -292,8 +303,8 @@ private:
   // The dimesions to control. In the command frame. [x, y, z, roll, pitch, yaw]
   std::array<bool, 6> control_dimensions_ = { { true, true, true, true, true, true } };
 
-  // latest_state_mutex_ is used to protect the state below it
-  mutable std::mutex latest_state_mutex_;
+  // input_mutex_ is used to protect the state below it
+  mutable std::mutex input_mutex_;
   Eigen::Isometry3d tf_moveit_to_robot_cmd_frame_;
   Eigen::Isometry3d tf_moveit_to_ee_frame_;
   geometry_msgs::TwistStampedConstPtr latest_twist_stamped_;
@@ -302,5 +313,9 @@ private:
   ros::Time latest_joint_command_stamp_ = ros::Time(0.);
   bool latest_nonzero_twist_stamped_ = false;
   bool latest_nonzero_joint_cmd_ = false;
+
+  // input condition variable used for low latency mode
+  std::condition_variable input_cv_;
+  bool new_input_cmd_ = false;
 };
 }  // namespace moveit_servo
