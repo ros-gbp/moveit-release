0f41d2e62576cadbfcb18416c68582b07c128e1e
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2021-11-06 15:50:24 +0100

    Rebase from 'upstream'

--- moveit_ros/occupancy_map_monitor/src/occupancy_map_monitor.cpp
+++ moveit_ros/occupancy_map_monitor/src/occupancy_map_monitor.cpp
@@ -37,7 +37,7 @@
 #include <ros/ros.h>
 #include <moveit_msgs/SaveMap.h>
 #include <moveit_msgs/LoadMap.h>
-#include <moveit/occupancy_map_monitor/occupancy_map.h>
+#include <moveit/collision_detection/occupancy_map.h>
 #include <moveit/occupancy_map_monitor/occupancy_map_monitor.h>
 #include <XmlRpcException.h>
 
@@ -99,7 +99,7 @@ void OccupancyMapMonitor::initialize()
                                                      << "\" specified but no TF instance (buffer) specified. "
                                                         "No transforms will be applied to received data.");
 
-  tree_.reset(new OccMapTree(map_resolution_));
+  tree_ = std::make_shared<collision_detection::OccMapTree>(map_resolution_);
   tree_const_ = tree_;
 
   XmlRpc::XmlRpcValue sensor_list;
@@ -133,8 +133,8 @@ void OccupancyMapMonitor::initialize()
           {
             try
             {
-              updater_plugin_loader_.reset(new pluginlib::ClassLoader<OccupancyMapUpdater>(
-                  "moveit_ros_perception", "occupancy_map_monitor::OccupancyMapUpdater"));
+              updater_plugin_loader_ = std::make_unique<pluginlib::ClassLoader<OccupancyMapUpdater>>(
+                  "moveit_ros_perception", "occupancy_map_monitor::OccupancyMapUpdater");
             }
             catch (pluginlib::PluginlibException& ex)
             {
